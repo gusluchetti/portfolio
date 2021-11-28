8e1df199844e5560234fe706da4aab08162fc050
Gustavo Luchetti <gusta.luchetti@gmail.com>
2021-11-28 17:23:27 -0300

    fix: index page now works

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
-import { ColorModeScript } from '@chakra-ui/react';
 import * as React from 'react';
-import ReactDOM from 'react-dom';
 import { App } from '../pages/App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const IndexPage = () => {
+  return (
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+};
+
+export default IndexPage;
