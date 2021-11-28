52c4f6b4a8b0ab2f1c8c8aae1b8466bd985cc384
Gustavo Luchetti <gusta.luchetti@gmail.com>
2021-11-28 16:00:02 -0300

    added chakra-ui

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,7 @@ module.exports = {
     title: "luchetti",
   },
   plugins: [
+    "@chakra-ui/gatsby-plugin",
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
