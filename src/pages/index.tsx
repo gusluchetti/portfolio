66d9cd9c673b7a7f34aca8c3c4846abafc0d4810
Gustavo Luchetti <gusta.luchetti@gmail.com>
2021-12-02 22:50:53 -0300

    feat: updated tsconfig

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,13 @@
-import * as React from 'react';
-import { App } from '../pages/App';
-import { ChakraProvider } from '@chakra-ui/react';
+import * as React from 'react'
+import { App } from '../pages/App'
+import { ChakraProvider } from '@chakra-ui/react'
 
 const IndexPage = () => {
   return (
     <ChakraProvider>
       <App />
     </ChakraProvider>
-  );
-};
+  )
+}
 
-export default IndexPage;
+export default IndexPage
