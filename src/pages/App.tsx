92cb74b1c205517457c29bc23fe0f6252261dca7
Gustavo Luchetti <gustavo@luchetti.dev>
2022-09-12 03:35:59 +0200

    feat: update about page

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,17 +1,20 @@
 import React from 'react'
-import { Container, Heading, Text, Stack } from '@chakra-ui/react'
+import { Container, Divider, Heading, Text, Stack } from '@chakra-ui/react'
 import Navbar from '../components/navbar'
 
 const App = () => (
   <Container py={8} px={[4, 16, 24]} maxW="container.xl">
-      <Heading>Luchetti - Developer</Heading>
+    <Navbar />
+    <Divider />
+
+    <Heading>Luchetti - Developer</Heading>
       <Stack direction={["column", "row"]}>
         <Text>
-        Hi! I'm Gustavo Luchetti, and this is my portfolio website. I'm 21 years old, and I'm currently doing a masters in Artificial Intelligence at Utrecht University, in the Netherlands. 
+        Hi! I'm Gustavo Luchetti, and this is my portfolio website. I'm 22 years old, and I'm currently doing a Masters in Artificial Intelligence at Utrecht University, in the Netherlands.
         </Text> 
         
         <Text>
-          This is a constant WIP, since my current goal is to work in data science, not web/full-stack development, so if there's anything incomplete, please check back later!
+          This is a constant WIP, since my current goal is to work with data-science related projects, not web/full-stack development, so if there's anything incomplete, please check back later!
         </Text>
     </Stack>
   </Container>
