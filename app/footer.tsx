7c31cc1060255c7e1fb68fddb9e4a4cfd8abaeb1
Gustavo Luchetti <gustavo@luchetti.dev>
2024-03-01 11:01:47 -0300

    feat: button to toggle dark/light mode

--- app/footer.tsx
+++ app/footer.tsx
@@ -1,6 +1,15 @@
+import { Theme, useTheme } from '~/utils/theme-provider';
+
 export function Footer() {
+    const [, setTheme] = useTheme();
+
+    const toggleTheme = () => {
+        setTheme((prevTheme) => (prevTheme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT));
+    };
+
     return (
         <footer className="border-stone-900">
+            <button onClick={toggleTheme}>toggle</button>
         </footer>
     )
 }
