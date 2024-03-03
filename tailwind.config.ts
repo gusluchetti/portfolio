50a7e6f86051a8fa22fbd7b9caeac2ddd20443cf
Gustavo Luchetti <gustavo@luchetti.dev>
2024-03-03 15:25:48 -0300

    feat: add new color pallette to tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,26 @@
 import type { Config } from 'tailwindcss'
 
 export default {
-    content: ['./app/**/*.{js,jsx,ts,tsx}'],
-    theme: {
-        extend: {},
+  content: ['./app/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+    colors: {
+      'cod-gray': {
+        '50': '#f7f7f6',
+        '100': '#e6e3e1',
+        '200': '#cdc7c2',
+        '300': '#ada39b',
+        '400': '#8b8076',
+        '500': '#70665c',
+        '600': '#595248',
+        '700': '#49433c',
+        '800': '#3c3933',
+        '900': '#34312d',
+        '950': '#1a1815',
+      },
+
     },
-    plugins: [],
-    darkMode: 'class'
+  },
+  plugins: [],
+  darkMode: 'class'
 } satisfies Config
