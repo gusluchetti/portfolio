d8f1713f895dd7f2f31215904209e22d104c25af
Gustavo Luchetti <gustavo@luchetti.dev>
2024-03-05 16:25:03 -0300

    feat: update index page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -9,11 +9,24 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <>
+    <div className="max-w-prose">
       <h2 className="text-lg font-bold">bio</h2>
-      <p>this is a test!</p>
+      <p>
+        Hi! My name is Gustavo Luchetti, a 23 year-old full-stack developer
+        currently based in São Paulo, Brazil. Besides full-stack, I've worked with
+        data integratiton for a bit, and studied Artificial Intelligence for a year.
+        To no one's surprise, I game a lot! I also go bouldering from time to time,
+        and I make a pretty good chocolate chip cookie.
+      </p>
       <h2 className="text-lg font-bold">purpose</h2>
-      <p>another test. wow!</p>
-    </>
+      <p>
+        I see this website as both journal and portfolio. It’s very easy these days
+        to consume content (or code) and to never give back, so this is my way of
+        remeding that.
+      </p>
+      <p>
+        My current goal is to write a blog post once a month, at the very least. I’ll try my best to stick to that schedule.
+      </p>
+    </div>
   );
 }
