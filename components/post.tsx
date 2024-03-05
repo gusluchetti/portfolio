a55fcc9c0e079268bcdbb0c1f5369da52bd9b9c5
Gustavo Luchetti <gustavo@luchetti.dev>
2024-03-05 16:24:55 -0300

    feat: update article css

--- components/post.tsx
+++ components/post.tsx
@@ -4,14 +4,14 @@ import type { PostMeta } from "~/.server/posts";
 
 export const Post = ({ slug, frontmatter }: PostMeta) => {
   return (
-    <article>
+    <article className="border border-style rounded-md">
       <Link to={`/blog/${slug}`}>
         <h3>{frontmatter.title}</h3>
+        <p className="text-gray-600">{frontmatter.description}</p>
+        <time dateTime={frontmatter.published} >
+          {frontmatter.published}
+        </time>
       </Link>
-      <p className="text-gray-600">{frontmatter.description}</p>
-      <time dateTime={frontmatter.published} >
-        {frontmatter.published}
-      </time>
     </article>
   );
 };
