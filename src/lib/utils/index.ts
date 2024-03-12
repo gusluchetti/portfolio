26a1edf45f6b63281e6ce287dec870e82d231967
Gustavo Luchetti <gustavo@luchetti.dev>
2024-03-12 17:45:54 -0300

    feat: add custom types for metadata and posts

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,10 +1,27 @@
+import type { SvelteComponent } from "svelte"
+
+export type Metadata = {
+  title: string,
+  description: string,
+  date: Date,
+  updated: Date,
+  draft: boolean
+}
+
+export type Post = {
+  meta: Metadata,
+  path: string,
+  default: SvelteComponent
+}
+
 export const fetchMarkdownPosts = async () => {
   const allPostFiles = import.meta.glob('/src/routes/blog/*.md');
   const iterablePostFiles = Object.entries(allPostFiles);
 
   const allPosts = await Promise.all(
     iterablePostFiles.map(async ([path, resolver]) => {
-      const { metadata } = await resolver();
+      const file: any = await resolver();
+      const { metadata } = file;
       const postPath = path.slice(11, -3);
 
       return {
