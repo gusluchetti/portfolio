f0bb37f4175128dce8398061fff3680d9fcb1013
Gustavo Luchetti <gustavo@luchetti.dev>
2024-03-12 18:20:05 -0300

    feat: update style for header nav

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,15 +3,15 @@
 	export const active = false;
 </script>
 
-<header class="italic flex flex-row gap-5 border border-white">
-	<nav>
+<header class="italic flex flex-row gap-5">
+	<nav class="*:navlink">
 		<a class:active href="/">home</a>
 		<a class:active href="/blog">posts</a>
 		<a class:active href="/about">about</a>
 	</nav>
 </header>
 
-<main>
+<main class="p-3">
 	<slot />
 </main>
 
