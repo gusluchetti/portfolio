deba795e126ae5e8e4284bee08bb46879b1d8bca
Gustavo Luchetti <gustavo@luchetti.dev>
2024-03-12 17:44:49 -0300

    feat: better navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,20 @@
-<header>
+<script lang="ts">
+	import '../app.css';
+	export const active = false;
+</script>
+
+<header class="italic flex flex-row gap-5 border border-white">
 	<nav>
-		<a href="/">home</a>
-		<a href="/blog">blog posts</a>
-		<a href="/about">about</a>
+		<a class:active href="/">home</a>
+		<a class:active href="/blog">posts</a>
+		<a class:active href="/about">about</a>
 	</nav>
 </header>
-<slot />
-<footer>
+
+<main>
+	<slot />
+</main>
+
+<footer class="italic flex flex-row gap-5 border border-white">
 	<a href="https://github.com/gusluchetti/portfolio">view source</a>
 </footer>
