739ef4965e27889e21458a9109599a8a9ebd2966
Gustavo Luchetti <gustavo@luchetti.dev>
2024-03-12 14:57:03 -0300

    feat: add home page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<header>
+	<nav>
+		<a href="/">home</a>
+		<a href="/blog">blog posts</a>
+		<a href="/about">about</a>
+	</nav>
+</header>
+<slot />
+<footer>
+	<a href="https://github.com/gusluchetti/portfolio">view source</a>
+</footer>
