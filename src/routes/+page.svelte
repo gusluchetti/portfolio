8da266d7876fcdeeeec62827c3bceea89db13752
Gustavo Luchetti <gustavo@luchetti.dev>
2024-03-12 18:20:11 -0300

    feat: heading on landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
 	<meta name="description" content="a collection of all blog posts i've made so far" />
 </svelte:head>
 
-<p>hi! i code for a living as a full-stack dev</p>
+<h2>hi! i code for a living as a full-stack dev</h2>
 <p>currently based in São Paulo, Brazil</p>
 <br />
 <p>i'm always trying to make my workflow better/more reliable</p>
