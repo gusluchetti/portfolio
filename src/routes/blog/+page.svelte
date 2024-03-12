f7c349f5c090986d044fad931619dd7a763266d1
Gustavo Luchetti <gustavo@luchetti.dev>
2024-03-12 14:56:53 -0300

    feat: add main blog index and load function

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,2 +1,16 @@
-<a href="/">home</a>
+<script lang="ts">
+	import type { PageData } from './$types';
+	export let data: PageData;
+	console.log(data.posts);
+</script>
+
 <p>lots of blog posts!</p>
+<ul>
+	{#each data.posts as post}
+		<li>
+			<a href={post.path}>
+				{post.meta.title}
+			</a>
+		</li>
+	{/each}
+</ul>
