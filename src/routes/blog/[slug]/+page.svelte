9eb7586f83f4226d5d9b647eb6f05d58aa2f4a3e
Gustavo Luchetti <gustavo@luchetti.dev>
2024-03-12 14:56:22 -0300

    feat: get single blog post and page layout

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 	export let data: PageData;
+	const { title, date, Content } = data;
 </script>
 
-<a href="/">home</a>
-<h1>{data.title}</h1>
-<div>{@html data.content}</div>
+<h1>{title}</h1>
+<p>published: {date}</p>
+<Content />
