9b020e37707e7eeac0eb3ae466d506b0ef89e0be
Gustavo Luchetti <gustavo@luchetti.dev>
2024-03-12 14:57:16 -0300

    feat: conf for cloudflare pages

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,13 +1,23 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
-	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface PageState {}
-		// interface Platform {}
-	}
+  namespace App {
+    interface Locals { }
+
+    interface Platform {
+      env: {
+        COUNTER: DurableObjectNamespace;
+      };
+      context: {
+        waitUntil(promise: Promise<any>): void;
+      };
+      caches: CacheStorage & { default: Cache }
+    }
+
+    interface Session { }
+
+    interface Stuff { }
+  }
 }
 
-export {};
+export { };
