84e86ce2e44b5137a8e46d487b92c8bd678e8204
Gustavo Luchetti <gustavo@luchetti.dev>
2024-03-12 11:24:07 -0300

    feat: home and blog routes

---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,2 @@
+<a href="/">home</a>
+<p>lots of blog posts!</p>
---
+++ src/routes/blog/[slug]/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+	export let data: PageData;
+</script>
+
+<a href="/">home</a>
+<h1>{data.title}</h1>
+<div>{@html data.content}</div>
