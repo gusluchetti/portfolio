3d8a0ced6c05508e485b48568e70dda5e1495a91
Gustavo Luchetti <gustavo@luchetti.dev>
2024-03-13 10:42:12 -0300

    feat: init cv page

---
+++ src/routes/cv/+page.svelte
@@ -0,0 +1,74 @@
+<svelte:head>
+	<title>cv</title>
+	<meta name="description" content="a printable cv page. try it out!" />
+</svelte:head>
+
+<div class="max-w-prose text-sm">
+	<h2>Work Experience:</h2>
+	<h3>Source.ag | Amsterdam, The Netherlands</h3>
+	<ul>
+		<li>Implementation Analyst (Aug/2022 – Feb/2023)</li>
+		<p>
+			Integration and onboarding of new clients into the company (Excel, PostgreSQL), working
+			alongside the data platform team, automation and improvement of existing processes (Python w/
+			Pandas, Web APIs).
+		</p>
+	</ul>
+
+	<h3>BTG Pactual Digital | São Paulo, Brazil</h3>
+	<ul>
+		<li>Intern Developer (Jun/2021 – Oct/2021)</li>
+		<li>Assistant Analyst (Oct/2021 – Jan/2022)</li>
+	</ul>
+	<p>
+		Web development for back-office applications, front-end using Angular, back-end API and services
+		using .NET CORE with AWS Lambda, according to micro-services architecture.
+	</p>
+
+	<h3>Valemobi Consultoria Empresarial S/A | São Paulo, Brazil</h3>
+	<ul>
+		<li>- Intern Full-Stack Developer (Mar/2020 - Mar/2021)</li>
+	</ul>
+	<p>
+		Development of software for the stock market, prototyping of interfaces, creation of pages using
+		Angular, web services and maintenance of batch jobs on Kotlin and PostgreSQL, all according to
+		object-oriented programming.
+	</p>
+
+	<h2>Proficiencies:</h2>
+	<ul>
+		<li>Great team-player.</li>
+		<li>Eager to learn new technologies, and capable of analyzing and optimizing processes.</li>
+		<li>Git, Pandas, Angular, AWS, Flask, .NET Core, Node.js.</li>
+		<li>Python, Typescript, C#, Kotlin, Java, Go, PL/SQL.</li>
+	</ul>
+
+	<h2>Academic Background:</h2>
+
+	<ul>
+		<li>
+			<h4>[In-Person] Master’s at Utrecht University,</h4>
+			<p>Artificial Intelligence (Incomplete) Feb/2022 – Feb/2023.</p>
+		</li>
+		<li>
+			<h4>[In-Person] Bachelor’s at Escola Superior de Propaganda e Marketing (ESPM),</h4>
+			<p>Information Systems (Complete) Dec/2021.</p>
+		</li>
+	</ul>
+
+	<h2>Extracurricular Activities:</h2>
+	<ul>
+		<li>
+			Electives: Applied Artificial Intelligence, Machine Learning Analytics and Advanced Analytics
+			(ESPM) | Data Mining, Pattern Set Mining (UU)
+		</li>
+		<li>Team Captain (CS:GO) / Esports Management – Overbite ESPM, 2019-2021.</li>
+		<li>Game Designer / Developer - GameLab ESPM, 2020.</li>
+	</ul>
+
+	<h2>Languages:</h2>
+	<ul>
+		<li>Portuguese (Native)</li>
+		<li>English (Fluent) -> TOEFL 115/120, Aug 2021.</li>
+	</ul>
+</div>
