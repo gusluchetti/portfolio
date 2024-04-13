40d76dad1f294ecacb0fcb15e296372266cb2e01
Gustavo Luchetti <gustavo@luchetti.dev>
2024-04-13 16:30:42 -0300

    feat: navbar and css reset

--- app/root.tsx
+++ app/root.tsx
@@ -1,10 +1,18 @@
 import {
   Links,
   Meta,
+  NavLink,
   Outlet,
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import type { LinksFunction } from "@remix-run/cloudflare";
+
+import styles from './styles.css?url'
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: styles },
+];
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -25,5 +33,15 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
-  return <Outlet />;
+  return (
+    <>
+      <nav>
+        <ul>
+          <NavLink to="/">home</NavLink>
+          <NavLink to="/about">about</NavLink>
+        </ul>
+      </nav>
+      <Outlet />
+    </>
+  )
 }
