ce83aaf099dc28a13ca84f8e2406a10366712939
Gustavo Luchetti <gustavo@luchetti.dev>
2024-04-13 15:55:04 -0300

    cleanup main

--- src/routes/+page.svelte
+++
@@ -1,9 +0,0 @@
-<svelte:head>
-	<title>luchetti.dev</title>
-	<meta name="description" content="a collection of all blog posts i've made so far" />
-</svelte:head>
-
-<h2>hi! i code for a living as a full-stack dev</h2>
-<p>currently based in São Paulo, Brazil</p>
-<br />
-<p>i'm always trying to make my workflow better/more reliable</p>
