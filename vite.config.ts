c73ba6df9065983a21b1931c38086520145e5f7b
Gustavo Luchetti <gustavo@luchetti.dev>
2024-04-21 18:57:41 -0300

    fix: extra configuration of mdx files
    as per remix's documentation regarding migrating to vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,7 @@
+import mdx from '@mdx-js/rollup';
+import remarkFrontmatter from "remark-frontmatter";
+import remarkMdxFrontmatter from "remark-mdx-frontmatter";
+
 import { vitePlugin as remix } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
@@ -6,5 +10,13 @@ import tsconfigPaths from "vite-tsconfig-paths";
 installGlobals();
 
 export default defineConfig({
-  plugins: [remix(), tsconfigPaths()],
+  plugins: [
+    mdx({
+      remarkPlugins: [
+        remarkFrontmatter,
+        remarkMdxFrontmatter,
+      ]
+    }),
+    remix(),
+    tsconfigPaths()],
 });
