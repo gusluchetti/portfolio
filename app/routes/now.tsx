b2ea908658d1a805de56c2da21c664e3e5d8f12d
Gustavo Luchetti <gustavo@luchetti.dev>
2024-04-22 18:34:20 -0300

    feat: update now page content

--- app/routes/now.tsx
+++ app/routes/now.tsx
@@ -1,4 +1,4 @@
-import type { MetaFunction } from "@remix-run/react";
+import { type MetaFunction } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -13,11 +13,27 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div>
-      <h1>now</h1>
+      <section className="now__title">
+        <h1>nowdays</h1>
+        <h4 className="blog__subtitle">last_update: 2024-04-22 | <a style={{ opacity: '40%' }} href="https://sive.rs/nowff">what's a now page?</a>
+        </h4>
+      </section>
       <p>
-        so, i spent quite a bit of time reworking the website. i don't think the time spent was worth it, but i'm just glad
-        that i managed to finally finish it.
+        so, the keen-eyed among you might notice the website's looking a bit different.
+        no surprise that i managed to go down the web dev rabbit hole, and pretty much remade the website.
       </p>
+      <p>
+        although i'm not super happy with having spent 20+ hours on this project (again), i'm very happy with the initial results,
+        and i'm glad that i got acquainted with the 'latest' ways of developing and hosting a website.
+        there's more info on the technical details of the overhaul on the <a href="https://github.com/gusluchetti/portfolio">repo's</a> README.
+        might even write something up later. for now, i'd rather close this page and work on something new.
+      </p>
+      <br />
+      <p>
+        besides that, all is well! i've recently updated my workflow to finally include <a href="https://github.com/tmux/tmux/wiki">tmux</a>,
+        and that couldn't have gone better. everything feels seamless and the only thing stopping my progress is how fast i can type/write commands down.
+      </p>
+      <p>other than that, no big news! i want to ride this wave of productivity i've been feeling the last couple of days to get cool stuff done.</p>
       <div className="now__grid">
         <section>
           <h3>books</h3>
