16709e0f882a9d6a3c94bc64dcf35f9f5301df09
Gustavo Luchetti <gustavo@luchetti.dev>
2024-04-22 12:06:59 -0300

    feat: remove drafts in production

--- app/.server/post-utils.ts
+++ app/.server/post-utils.ts
@@ -3,6 +3,11 @@ import fs from 'node:fs/promises';
 import path from 'path';
 
 export const getAllPosts = () => {
+  let showDrafts = true;
+  if (process.env.NODE_ENV == 'production') {
+    showDrafts = false;
+  }
+
   const fileData = import.meta.glob(
     "./posts/*.mdx",
     { eager: true }
@@ -10,14 +15,15 @@ export const getAllPosts = () => {
   let posts: [string, unknown][] = Object.entries(fileData);
   posts = posts.filter((p) => {
     const [path, data]: [path: string, data: any] = p;
-    return data.frontmatter.draft === false
+    if (showDrafts) { return true; }
+    else {
+      return data.frontmatter.draft === false;
+    }
   })
   return posts;
 }
 
 export const getPostContent = async (filename: string) => {
-  console.log(`at server get post, path: ${filename}`)
-
   const relativePath = path.join(process.cwd(), `app/.server/posts/${filename}.mdx`);
   try {
     const fileContent = await fs.readFile(relativePath, { encoding: 'utf8' });
