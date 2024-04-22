8f4b96dc9bba27ad59272dfd699217b197c42541
Gustavo Luchetti <gustavo@luchetti.dev>
2024-04-22 12:35:03 -0300

    feat: define unplugin local font face import

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,37 @@
+import { installGlobals } from "@remix-run/node";
+
+import { defineConfig } from "vite";
+import { vitePlugin as remix } from "@remix-run/dev";
+import tsconfigPaths from "vite-tsconfig-paths";
+
 import mdx from '@mdx-js/rollup';
 import remarkFrontmatter from "remark-frontmatter";
 import remarkMdxFrontmatter from "remark-mdx-frontmatter";
 
-import { vitePlugin as remix } from "@remix-run/dev";
-import { installGlobals } from "@remix-run/node";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import Unfonts from 'unplugin-fonts/vite';
 
 installGlobals();
 
 export default defineConfig({
   plugins: [
+    Unfonts({
+      custom: {
+        families: [{
+          name: 'Writer',
+          local: 'Writer',
+          src: './assets/fonts/*.woff2',
+        }],
+        display: 'auto',
+        preload: true,
+        prefetch: false,
+        /**
+         * define where the font load tags should be inserted
+         * default: 'head-prepend'
+         *   values: 'head' | 'body' | 'head-prepend' | 'body-prepend'
+         */
+        injectTo: 'head-prepend',
+      },
+    }),
     mdx({
       remarkPlugins: [
         remarkFrontmatter,
